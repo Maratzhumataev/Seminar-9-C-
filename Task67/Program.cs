4ed513cbb44fda2bf186e45785fb355597bddbbf
Marat Zhumataev <marat.air.92@mail.ru>
2023-05-23 15:21:41 +0300

    Добавили задачи семинара

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = DigitsSum (number);
+Console.WriteLine(sum);
+
+int DigitsSum(int n)
+ {
+    if (n == 0) return 0;
+    return n % 10 + DigitsSum(n/10);
+}
---
+++ Task69/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 69. Напишите программу, которая возводит А в целую степень Б.
+
+Console.WriteLine("Введите целое число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PowNumber(numberA, numberB));
+
+int PowNumber (int numA, int numB)
+{
+    if(numB == 0) return 1;
+    return numA * PowNumber(numA, numB -1);
+}
