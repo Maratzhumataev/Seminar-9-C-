5d225a3f50796124539d1432a6927070b6eece78
MARAT ZHUMATAEV <ZhumataevMD@MacBook-Pro-MARAT.local>
2023-05-27 13:01:48 +0300

    Добавили задачи ДЗ

---
+++ Task66/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (numberM == numberN)
+    {
+        Console.WriteLine("Числа M и N равны");
+    }
+else
+{
+    SumFromMtoN (numberM, numberN);
+}
+
+void SumFromMtoN (int numM, int numN)
+{
+    if (numM == numN) 
+    {
+        sum += numM;
+        Console.WriteLine($"Сумма чисел равна {sum}");
+        return;
+    }
+    else
+    if (numM < numN)
+    {
+        sum += numM;
+        SumFromMtoN (numM+1, numN);
+    }
+    else
+    if (numM > numN)
+    {
+        sum += numN;
+        SumFromMtoN (numM, numN+1);
+    }
+    }
---
+++ Task68/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите неотрицательное число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if ((numberM < 0) & (numberN < 0)) Console.WriteLine ("Введено отрицательное число");
+else 
+{
+    int result = AccermanFunction(numberM, numberN);
+    Console.WriteLine(result);
+}
+
+int AccermanFunction (int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else
+        if ((numM > 0) && (numN == 0))
+        {
+            return AccermanFunction (numM-1, 1);
+        }
+    else
+        {
+            return AccermanFunction (numM - 1, AccermanFunction (numM, numN-1));
+        }
+}
